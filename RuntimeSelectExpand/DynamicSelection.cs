fbc759738bb531548095a9318cefb217ee78bee5
Kleber <kleber.hermsdorff@gmail.com>
2015-07-11 19:32:59 -0300

    Included basic queries using Expression

--- RuntimeSelectExpand/DynamicSelection.cs
+++ RuntimeSelectExpand/DynamicSelection.cs
@@ -12,7 +12,41 @@ namespace RuntimeSelectExpand
         public static IQueryable Select(IQueryable source, Type type)
         {
             ParameterExpression sourceItem = Expression.Parameter(source.ElementType, "t");
-            IEnumerable<MemberBinding> bindings = type.GetFields().Select(p => Expression.Bind(p, Expression.Property(sourceItem, sourceProperties[p.Name])))
+
+            var bindings = new List<MemberBinding>();
+
+            BindSystemTypeProperties(type, sourceItem, bindings);
+
+            foreach (var property in type.GetFields().Where(p => p.FieldType.Namespace != "System"))
+            {
+                var innerBindings = new List<MemberBinding>();
+                BindSystemTypeProperties(property.FieldType, Expression.Property(sourceItem, property.Name), innerBindings);
+                
+                var innerMemberBind = Expression.Bind(
+                    property,
+                    Expression.MemberInit(
+                        Expression.New(property.FieldType), innerBindings));
+
+                bindings.Add(innerMemberBind);
+            }
+
+            Expression selector = Expression.Lambda(
+                Expression.MemberInit(
+                    Expression.New(type.GetConstructor(Type.EmptyTypes)), bindings), sourceItem);
+
+            return source.Provider.CreateQuery(Expression.Call(typeof(Queryable), "Select", new Type[] { source.ElementType, type },
+                         Expression.Constant(source), selector));
+
+        }
+
+        private static void BindSystemTypeProperties(Type type, Expression sourceItem, List<MemberBinding> bindings)
+        {
+            foreach (var property in type.GetFields().Where(p => p.FieldType.Namespace == "System"))
+            {
+                var propertyName = property.Name;
+                var member = Expression.Bind(property, Expression.Property(sourceItem, propertyName));
+                bindings.Add(member);
+            }
         }
     }
 }
--- Testes/QueryTest.cs
+++ Testes/QueryTest.cs
@@ -17,7 +17,7 @@ namespace Testes
         public void ExecuteQueryOverACollection()
         {
             // arrange
-            const string Query = "$select=Id,Products/Name&$expand=Products";
+            const string Query = "$select=Id";
             var parser = new ODataParser();
             var tree = parser.Parse(Query);
             tree.Bind(typeof(Category));
@@ -26,13 +26,43 @@ namespace Testes
             var categories = new List<Category>
                 {
                     new Category
-                        { Id = 1, Name = "C1", Products = new List<Product> { new Product { Id = 1, Name = "P1" } } }
+                        { Id = 1, Name = "C1", Products = new List<Product> { new Product { Id = 1, Name = "P1" } } },
+                    new Category
+                        { Id = 2, Name = "C2", Products = new List<Product> { new Product { Id = 2, Name = "P2" } } }
                 };
 
             // act
             var selection = DynamicSelection.Select(categories.AsQueryable(), tree.QueryType);
 
             // assert
+            const string expression = "System.Collections.Generic.List`1[Testes.Fakes.Category].Select(t => new {Id:Int32}() {Id = t.Id})";
+            Assert.AreEqual(expression, selection.ToString());
+        }
+
+        [TestMethod]
+        public void ExecuteQueryOverACollectionWithComplexProperty()
+        {
+            // arrange
+            const string Query = "$select=Id,Manufacturer/Name";
+            var parser = new ODataParser();
+            var tree = parser.Parse(Query);
+            tree.Bind(typeof(Product));
+            tree.BuildType();
+
+            var products = new List<Product>
+                {
+                    new Product
+                        { Id = 1, Name = "C1", Manufacturer = new Manufacturer{Id = 1, Name="M1"}},
+                    new Product
+                        { Id = 2, Name = "C2", Manufacturer = new Manufacturer{Id = 1, Name="M2"}}
+                };
+
+            // act
+            var selection = DynamicSelection.Select(products.AsQueryable(), tree.QueryType);
+
+            // assert
+            const string expression = "System.Collections.Generic.List`1[Testes.Fakes.Product].Select(t => new {Id:Int32;Manufacturer:{Name:String}}() {Id = t.Id, Manufacturer = new {Name:String}() {Name = t.Manufacturer.Name}})";
+            Assert.AreEqual(expression, selection.ToString());
         }
     }
 }
