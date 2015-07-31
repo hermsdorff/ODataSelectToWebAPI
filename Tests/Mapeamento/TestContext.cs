b1bce918b19f81d92b127a6117b7e1f6e0278f1b
Kleber <kleber.hermsdorff@gmail.com>
2015-07-31 07:07:21 -0300

    Started tests with entity framework

---
+++ Tests/Mapeamento/TestContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Tests.Mapeamento
+{
+    class TestContext : DbContext
+    {
+        public TestContext(DbConnection connection) : base(connection, true)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Tests/QueryEntity.cs
@@ -0,0 +1,25 @@
+﻿using Effort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+
+namespace Tests
+{
+    [TestClass]
+    class QueryEntity
+    {
+        [TestMethod]
+        public void ExecuteQueryOverACollection()
+        {
+            // arrange
+            DbConnection conn = DbConnectionFactory.CreateTransient();
+            var context = new TestContext(conn);
+
+            // act
+            // assert
+        }
+    }
+}
